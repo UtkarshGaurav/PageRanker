8dd9aa7c09ca71943d4ea0d00a7b05685a1220a4
Utkarsh Gaurav <ugaurav.rishu@gmail.com>
2021-08-18 20:13:23 +0530

    Initial commit

---
+++ sprank.py
@@ -0,0 +1,102 @@
+import sqlite3
+
+conn = sqlite3.connect('spider.sqlite')
+cur = conn.cursor()
+
+# Finding the ids that send page rank - we are only interested
+# in pages in the SCC that contain in and out links
+cur.execute('''SELECT DISTINCT from_id FROM Links''')
+from_ids = list()
+for row in cur: 
+    from_ids.append(row[0])
+
+# Finding the ids that receive page rank
+to_ids = list()
+links = list()
+cur.execute('''SELECT DISTINCT from_id, to_id FROM Links''')
+for row in cur:
+    from_id = row[0]
+    to_id = row[1]
+    if from_id == to_id : continue
+    if from_id not in from_ids : continue
+    if to_id not in from_ids : continue
+    links.append(row)
+    if to_id not in to_ids : to_ids.append(to_id)
+
+# Retrieving latest page ranks for strongly connected components
+prev_ranks = dict()
+for node in from_ids:
+    cur.execute('''SELECT new_rank FROM Pages WHERE id = ?''', (node, ))
+    row = cur.fetchone()
+    prev_ranks[node] = row[0]
+
+sval = input('How many iterations:')
+many = 1
+if ( len(sval) > 0 ) : many = int(sval)
+
+# Sanity check
+if len(prev_ranks) < 1 : 
+    print("Nothing to page rank.  Check data.")
+    quit()
+
+# Running page rank in the memory so that it is faster
+for i in range(many):
+    # print prev_ranks.items()[:5]
+    next_ranks = dict();
+    total = 0.0
+    for (node, old_rank) in list(prev_ranks.items()):
+        total = total + old_rank
+        next_ranks[node] = 0.0
+    # printing the total
+
+    # Finding the number of outbound links and sending the page rank down each of them
+    for (node, old_rank) in list(prev_ranks.items()):
+        # printing node, old_rank
+        give_ids = list()
+        for (from_id, to_id) in links:
+            if from_id != node : continue
+           #  printing '   ',from_id,to_id
+
+            if to_id not in to_ids: continue
+            give_ids.append(to_id)
+        if ( len(give_ids) < 1 ) : continue
+        amount = old_rank / len(give_ids)
+        # printing node, old_rank,amount, give_ids
+    
+        for id in give_ids:
+            next_ranks[id] = next_ranks[id] + amount
+    
+    newtot = 0
+    for (node, next_rank) in list(next_ranks.items()):
+        newtot = newtot + next_rank
+    evap = (total - newtot) / len(next_ranks)
+
+    # printing newtot, evap
+    for node in next_ranks:
+        next_ranks[node] = next_ranks[node] + evap
+
+    newtot = 0
+    for (node, next_rank) in list(next_ranks.items()):
+        newtot = newtot + next_rank
+
+    # Computing the per-page avg change from old rank to new rank
+    totdiff = 0
+    for (node, old_rank) in list(prev_ranks.items()):
+        new_rank = next_ranks[node]
+        diff = abs(old_rank-new_rank)
+        totdiff = totdiff + diff
+
+    avediff = totdiff / len(prev_ranks)
+    print(i+1, avediff)
+
+    # rotating
+    prev_ranks = next_ranks
+
+# Entering the final ranks back into the database
+print(list(next_ranks.items())[:5])
+cur.execute('''UPDATE Pages SET old_rank=new_rank''')
+for (id, new_rank) in list(next_ranks.items()) :
+    cur.execute('''UPDATE Pages SET new_rank=? WHERE id=?''', (new_rank, id))
+conn.commit()
+cur.close()
+
